3f34af36e24b03fb69150ab46bedd8f599668488
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-04-07 23:52:25 -0400

    basic next frontend for webapp

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <main>
+      <div>
+        <h1>
+          main page
+        </h1>
+      </div>
+    </main>
+  ) 
+}
+
+export default page
