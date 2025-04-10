7684b1e2f5368ec731b354a431540c28b24966a9
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-04-10 02:12:28 -0400

    more playing around

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 import os
+from pydantic import BaseModel
 from dotenv import load_dotenv
+from typing import List
 from scrapybara import Scrapybara
 from scrapybara.tools import BashTool, ComputerTool, EditTool
 from scrapybara.openai import OpenAI
@@ -9,6 +11,11 @@ from scrapybara.prompts import UBUNTU_SYSTEM_PROMPT
 
 load_dotenv()
 SCRAPYBARA_API_KEY = os.getenv("SCRAPYBARA_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# change this check later to have an LLM 
+
+if not OPENAI_API_KEY: 
+    pass
 
 client = Scrapybara(api_key=SCRAPYBARA_API_KEY)
 
@@ -17,14 +24,30 @@ instance = client.start_ubuntu(
     timeout_hours=1,
 )
 
+
+class HNSchema(BaseModel):
+    class Post(BaseModel):
+        title: str
+        url: str 
+        points: int
+    
+    posts: List[Post]
+
+stream_url = instance.get_stream_url().stream_url
+print(f"Stream URL: {stream_url}")
+
 cdp_url = instance.browser.start().cdp_url
 playwright = sync_playwright().start()
 browser = playwright.chromium.connect_over_cdp(cdp_url)
 
-prompt = ''''''
+prompt = f'''
+navigate to playtictactoe.com and play a game of tic tac toe with the computer.
+make sure to win the game.
+'''
 
 response = client.act(
-    model=OpenAI(),
+    model=OpenAI(
+    ),
     tools=[
         BashTool(instance),
         ComputerTool(instance),
@@ -33,6 +56,10 @@ response = client.act(
     system=UBUNTU_SYSTEM_PROMPT,
     prompt=f"{prompt}",
     on_step=lambda step: print(step.text),
+    schema=HNSchema
 )
+
+posts = response.output.posts
+print(posts)
 
 instance.stop()
